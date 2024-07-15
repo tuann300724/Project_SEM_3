56227add6afd0d72b65b057ca14277cf1bc1054c
nghimathit <vannghinguyen152001@gmail.com>
2024-07-15 14:19:19 +0700

    nghi

--- src/components/HouseForRent/index.js
+++ src/components/HouseForRent/index.js
@@ -1,9 +1,23 @@
 import React from "react";
+import styles from "./HouseForRent.module.scss";
+import classNames from "classnames/bind";
+import Searchsell from "../Aboutus/Searchsell";
 
+const cx = classNames.bind(styles);
 function HouseForRent(props) {
-  return <div>
-ddfcccccccccccccccccccdee
-         </div>;
+  return (
+    <div className={cx("wrapper")}>
+      <div className={cx("ads")}></div>
+
+      <div className={cx("wrapper-content")}>
+        <div className={cx("wrapper-search")}>
+          <Searchsell />
+        </div>
+      </div>
+
+      <div className={cx("ads")}></div>
+    </div>
+  );
 }
 
 export default HouseForRent;
