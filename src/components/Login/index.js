0b03d01be029cdde43cc691a4d6142bd5ad0e1a9
thanh phong <vonghung849@gmail.com>
2024-07-19 13:00:54 +0700

    fas

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -55,7 +55,6 @@ function Login(props) {
                 Tên đăng nhập không được để trống
               </div>
             </div>
-<<<<<<< HEAD
           </div>
           <div className={cx("layout-left")}>
             <div className={cx("layout-leftX2")}>
@@ -198,104 +197,6 @@ function Login(props) {
                       )}
                       {/* <svg
                         fontSize="24px"
-=======
-            <div className={cx("wapper-input", "pdtop")}>
-              <div className={cx("wapper-inputx2")}>
-                <div className={cx("wapper-icon")}>
-                  <svg
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    font-size="24px"
-                  >
-                    <path
-                      d="M18.7691 21H5.23076C4.90434 21 4.59129 20.8712 4.36048 20.642C4.12967 20.4128 4 20.1019 4 19.7778V11.2222C4 10.8981 4.12967 10.5872 4.36048 10.358C4.59129 10.1288 4.90434 10 5.23076 10H18.7691C19.0955 10 19.4086 10.1288 19.6394 10.358C19.8702 10.5872 19.9999 10.8981 19.9999 11.2222V19.7778C19.9999 20.1019 19.8702 20.4128 19.6394 20.642C19.4086 20.8712 19.0955 21 18.7691 21Z"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                    <path
-                      d="M7.42859 9.85709V6.99997C7.42859 5.93911 7.91022 4.9217 8.76752 4.17156C9.62482 3.42142 10.7876 3 12 3C13.2124 3 14.3751 3.42142 15.2324 4.17156C16.0897 4.9217 16.5714 5.93911 16.5714 6.99997V9.85709"
-                      stroke="currentColor"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    ></path>
-                  </svg>
-                </div>
-                <input
-                  autocomplete="username"
-                  name="username"
-                  placeholder="Mật Khẩu"
-                  type="password"
-                  mode="normal"
-                  className={cx("inputconentwp")}
-                />
-              </div>
-              <div className={cx("validate-input")} type="negative">
-                Mật Khẩu Không Được Để Trống
-              </div>
-            </div>
-            <button className={cx("wrapper-button")}>
-              <div className={cx("button-login")}>
-                <span type="primary" className={cx("logintext")}>
-                  Đăng nhập
-                </span>
-              </div>
-            </button>
-          </form>
-          <div className={cx("footerlogin")}>
-            <div
-              className={cx("wrapper-footerlogin")}
-              onClick={handelRemeberAccount}
-            >
-              {remeberAccunt ? (
-                <svg
-                  font-size="24px"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M17 4H7C5.34315 4 4 5.34315 4 7V17C4 18.6569 5.34315 20 7 20H17C18.6569 20 20 18.6569 20 17V7C20 5.34315 18.6569 4 17 4Z"
-                    stroke="currentColor"
-                    stroke-width="1.5"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
-              ) : (
-                <svg
-                  font-size="24px"
-                  width="1em"
-                  height="1em"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M17 4H7C5.34315 4 4 5.34315 4 7V17C4 18.6569 5.34315 20 7 20H17C18.6569 20 20 18.6569 20 17V7C20 5.34315 18.6569 4 17 4Z"
-                    fill="currentColor"
-                    stroke="currentColor"
-                    stroke-width="1.5"
-                    stroke-linejoin="round"
-                  ></path>
-                  <path
-                    d="M17 8L10.7 15L8 12"
-                    stroke="#fff"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  ></path>
-                </svg>
-              )}
-              {/* <svg
-                        font-size="24px"
->>>>>>> 538ccd5f06b23108cbbc950dab04c0bd497c8756
                         width="1em"
                         height="1em"
                         viewBox="0 0 24 24"
@@ -310,7 +211,6 @@ function Login(props) {
                         ></path>
                       </svg> */}
 
-<<<<<<< HEAD
                       <div className={cx("remenberAcount")}>
                         <div type="primary" className={cx("remenberAcountx2")}>
                           Nhớ tài khoản
@@ -513,11 +413,6 @@ function Login(props) {
                     </div>
                   </a>
                   {" "} tại đây
-=======
-              <div className={cx("remenberAcount")}>
-                <div type="primary" className={cx("remenberAcountx2")}>
-                  Nhớ tài khoản
->>>>>>> 538ccd5f06b23108cbbc950dab04c0bd497c8756
                 </div>
               </div>
             </div>
