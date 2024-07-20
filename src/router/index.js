f95644f0415a94fd815a7e46f22481d0eac060ae
jortboy2 <vonghung849@gmail.com>
2024-07-20 20:37:04 +0700

    new

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import DashBoard from "../Admin/Dashboard";
 import UserPost from "../User/UserPost";
 import Toolspage from "../components/Toolpage";
 import UserDashboard from "../User/UserDashboard";
+import Userpackage from "../User/UserPackage";
 const publicRoutes = [
   { path: "/", component: HomePage },
   { path: "/about", component: Aboutus },
@@ -18,10 +19,11 @@ const publicRoutes = [
   { path: "/new", component: New },
   { path: "/infopost", component: InfoPost },
   { path: "/usermanager/*", component: LayoutUser },
+  { path: "/tinh-lai-suat", component: Toolspage },
   {path : "/user", component: UserPost, layout: null},
   {path : "/user/dashboard", component: UserDashboard, layout: null},
   {path : "/user/post", component: UserPost, layout: null},
-  { path: "/tinh-lai-suat", component: Toolspage },
+  {path : "/user/package", component: Userpackage, layout: null},
 ];
 const privateRoutes = [
   { path: '/admin', component : DashBoard }
