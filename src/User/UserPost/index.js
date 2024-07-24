3f4d3b4eb1f8b452d9c57f771a934c8a1582a7f8
jortboy2 <vonghung849@gmail.com>
2024-07-24 15:45:35 +0700

    post

--- src/User/UserPost/index.js
+++ src/User/UserPost/index.js
@@ -12,6 +12,8 @@ function UserPost(props) {
   const [latitude, setLatitude] = useState(10.762622);
   const [longitude, setLongitude] = useState(106.660172);
   // data để post
+  const [datatypehouse, setDatatypeHouse] = useState([]);
+  const [datapurpose, setDatapurpose] = useState([]);
   const [title, setTitle] = useState();
   const [address, setAddress] = useState();
   const [zipcode, setZipcode] = useState();
@@ -20,19 +22,31 @@ function UserPost(props) {
   const [bedroom, setBedrooms] = useState(0);
   const [bathroom, setBathroom] = useState(0);
   const [description, setDescription] = useState();
-  const [status, setStatus] = useState(0);
+  const [status, setStatus] = useState("Pending");
   const [userid, setUserid] = useState(1);
   const [typehouse, setTypehouse] = useState();
   const [images, setImages] = useState([]);
-  const [purpose, setPurpost] = useState();
-  const [LegalStatus, setLegalStatus] = useState();
-  const [Contractor, setContractor] = useState();
+  const [isActive, setIsActive] = useState(false);
+  const [purpose, setPurpost] = useState("Bán");
+  const [LegalStatus, setLegalStatus] = useState("Sổ đổ/sổ hồng");
   const [username, setUsername] = useState();
   const [email, setEmail] = useState();
   const [phone, setPhone] = useState();
   const [PostInfo, setPostInfo] = useState([]);
+  const [error, setError] = useState("");
+
   const cx = classNames.bind(styles);
   const inputFileRef = useRef(null);
+
+  // get purpose
+  useEffect(() => {
+    axios
+      .get("http://localhost:5117/api/TypeHouse")
+      .then((result) => {
+        setDatatypeHouse(result.data.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   useEffect(() => {
     axios
@@ -41,7 +55,7 @@ function UserPost(props) {
         setCitys(result.data.data);
       })
       .catch((err) => console.log(err));
-  }, [citys]);
+  }, []);
   const HandleDistricts = (e) => {
     const idtinh = e.target.value;
     axios
@@ -86,45 +100,103 @@ function UserPost(props) {
       setImages((prevImages) => [...prevImages, ...newImages]);
     }
   };
+
   useEffect(() => {
     console.log(images);
     return () => {
       images && URL.revokeObjectURL(images.preview);
     };
   }, [images]);
-  const handleReducerBedroom = (e) =>{
+  const handleReducerBedroom = (e) => {
     e.preventDefault();
-    setBedrooms(bedroom - 1)
-    if(bedroom <= 0){
+    setBedrooms(bedroom - 1);
+    if (bedroom <= 0) {
       setBedrooms(0);
     }
-  }
-  const handleIncreaseBedroom = (e) =>{
+  };
+  const handleIncreaseBedroom = (e) => {
     e.preventDefault();
-    setBedrooms(bedroom + 1)
-  }
-  const handleReducerBathroom = (e) =>{
+    setBedrooms(bedroom + 1);
+  };
+  const handleReducerBathroom = (e) => {
     e.preventDefault();
-    setBathroom(bathroom - 1)
-    if(bathroom <= 0){
+    setBathroom(bathroom - 1);
+    if (bathroom <= 0) {
       setBathroom(0);
     }
-  }
-  const handleIncreaseBathroom = (e) =>{
+  };
+  const handleIncreaseBathroom = (e) => {
     e.preventDefault();
-    setBathroom(bathroom + 1)
-  }
+    setBathroom(bathroom + 1);
+  };
+  const validateForm = () => {
+    let errorMessages = [];
+
+    if (!title || title.length < 20) {
+        errorMessages.push("Tiêu đề phải có ít nhất 20 ký tự.");
+    }
+    if (!description || description.length < 50) {
+        errorMessages.push("Mô tả phải có ít nhất 50 ký tự.");
+    }
+    if (images.length < 4) {
+        errorMessages.push("Phải có ít nhất 4 ảnh.");
+    }
+    if (!address || !zipcode || !Price || !Area || bedroom <= 0 || bathroom <= 0 || !description || !title) {
+        errorMessages.push("Các trường bắt buộc không được để trống và số phòng ngủ, phòng tắm phải lớn hơn 0.");
+    }
+
+    if (errorMessages.length > 0) {
+        setError(errorMessages.join(" "));
+        return false;
+    }
+    return true;
+};
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    setPostInfo(e.target.value);
+    if (!validateForm()) {
+      return;
+    }
+    const formData = new FormData();
+
+    formData.append("Title", title);
+    formData.append("Address", fullcity.full_name);
+    formData.append("Zipcode", zipcode);
+    formData.append("Price", Price);
+    formData.append("Area", Area);
+    formData.append("Bedrooms", bedroom);
+    formData.append("Bathrooms", bathroom);
+    formData.append("Description", description);
+    formData.append("isActive", isActive);
+    formData.append("Status", status);
+    formData.append("LegalStatus", LegalStatus);
+    formData.append("typeHouseId", typehouse);
+    formData.append("UserId", userid);
+    images.forEach((img) => {
+      formData.append("formFiles", img);
+    });
+    axios
+      .post("http://localhost:5117/api/Post", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      })
+      .then((response) => {
+        console.log("Success:", response.data);
+      })
+      .catch((error) => {
+        console.error("Error:", error.response.data);
+      });
   };
-  useEffect(() =>{
-    console.log(PostInfo);
-  }, [PostInfo])
+
+  useEffect(() => {
+    console.log("Postinfo: ", PostInfo);
+  }, [PostInfo]);
   return (
     <div className={cx("container-post", "container-xl")}>
       <div className={cx("post-title")}>Thông tin cơ bản</div>
       <form action="#" onSubmit={handleSubmit}>
+      {error && <div className={cx("alert alert-danger")}>{error}</div>}
         <div className={cx("post-rent-sell")}>
           {/* <button className={cx("sell", "activebtn")}>Bán</button>
         <button className={cx("rent")}>Thuê</button> */}
@@ -133,7 +205,7 @@ function UserPost(props) {
             value={purpose}
             onChange={(e) => setPurpost(e.target.value)}
           >
-            <option value="thuê">Thuê</option>
+            <option value="Thuê">Thuê</option>
             <option value="Bán">Bán</option>
           </select>
         </div>
@@ -146,14 +218,25 @@ function UserPost(props) {
             value={typehouse}
             onChange={(e) => setTypehouse(e.target.value)}
           >
-            <option value="Bán chung cư mini">Bán chung cư mini</option>
-            <option value="Bán nhà riêng">Bán nhà riêng</option>
-            <option value="Bán đất">Bán đất</option>
-            <option value="Bán căn hộ chung cư">Bán căn hộ chung cư</option>
+            {datatypehouse
+              .filter((c) => c.purpose === purpose)
+              .map((item, index) => (
+                <option value={item.id} key={index}>
+                  {item.type}
+                </option>
+              ))}
           </select>
         </div>
-        <div className={cx("type-house-title")}>Chọn nhanh địa chỉ </div>
-        <AutocompleteAddress />
+        <div className={cx("type-house-title")}>Nhập Zipcode </div>
+        <div className={cx("area-input")}>
+          <input
+            type="number"
+            pattern="[0-9]"
+            value={zipcode}
+            onChange={(e) => setArea(e.target.value)}
+          />
+        </div>
+        {/* <AutocompleteAddress /> */}
         <div className={cx("type-group")}>
           <div className={cx("type-province-title")}>
             Tỉnh, thành phố <span className={cx("reddot")}>*</span>{" "}
@@ -200,7 +283,7 @@ function UserPost(props) {
           <input
             type="text"
             placeholder="Chọn"
-            value={fullcity.name}
+            value={fullcity.full_name}
             onChange={(e) => setAddress(e.target.value)}
           />
         </div>
@@ -282,19 +365,6 @@ function UserPost(props) {
             <option value="Đang chờ sổ">Đang chờ sổ</option>
           </select>
         </div>
-        <div className={cx("type-house-title")}>Nội thất</div>
-
-        <div className={cx("select-legal")}>
-          <select
-            name="Contractor"
-            value={Contractor}
-            onChange={(e) => setContractor(e.target.value)}
-          >
-            <option value="Đầy đủ">Đầy đủ</option>
-            <option value="Cơ bản">Cơ bản</option>
-            <option value="Không nội thất">Không nội thất</option>
-          </select>
-        </div>
         <div className={cx("select-bedroom")}>
           <span className={cx("text-bedroom")}>Số phòng ngủ</span>
           <span className={cx("btn-bedroom")}>
@@ -311,7 +381,11 @@ function UserPost(props) {
           <span className={cx("text-bathroom")}>Số phòng tắm</span>
           <span className={cx("btn-bathroom")}>
             <button onClick={handleReducerBathroom}>-</button>
-            <input type="text" value={bathroom} onChange={(e) => setBathroom(e.target.value)} />
+            <input
+              type="text"
+              value={bathroom}
+              onChange={(e) => setBathroom(e.target.value)}
+            />
             <button onClick={handleIncreaseBathroom}>+</button>
           </span>
         </div>
@@ -347,17 +421,17 @@ function UserPost(props) {
                 <path
                   d="M5.54 97.33H126.37"
                   stroke="#63666A"
-                  stroke-width="1"
+                  strokeWidth="1"
                   strokeMiterlimit="10"
-                  stroke-linecap="round"
+                  strokeLinecap="round"
                 ></path>
                 <path
                   d="M97 97.33H49.91V34.65C49.91 34.3848 50.0154 34.1305 50.2029 33.9429C50.3904 33.7554 50.6448 33.65 50.91 33.65H84.18C84.6167 33.6541 85.0483 33.7445 85.4499 33.9162C85.8515 34.0878 86.2152 34.3372 86.52 34.65L96.02 44.15C96.3321 44.4533 96.5811 44.8153 96.7527 45.2151C96.9243 45.615 97.0152 46.0449 97.02 46.48L97 97.33Z"
                   fill="#D7D7D7"
                   stroke="#63666A"
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
                 <path
                   d="M59.09 105.64H42.09C41.8248 105.64 41.5704 105.535 41.3829 105.347C41.1954 105.16 41.09 104.905 41.09 104.64V41.79C41.09 41.5248 41.1954 41.2705 41.3829 41.0829C41.5704 40.8954 41.8248 40.79 42.09 40.79H77.33L89 52.42V104.62C89 104.885 88.8946 105.14 88.7071 105.327C88.5196 105.515 88.2652 105.62 88 105.62H74.86"
@@ -366,72 +440,72 @@ function UserPost(props) {
                 <path
                   d="M59.09 105.64H42.09C41.8248 105.64 41.5704 105.535 41.3829 105.347C41.1954 105.16 41.09 104.905 41.09 104.64V41.79C41.09 41.5248 41.1954 41.2705 41.3829 41.0829C41.5704 40.8954 41.8248 40.79 42.09 40.79H77.33L89 52.42V104.62C89 104.885 88.8946 105.14 88.7071 105.327C88.5196 105.515 88.2652 105.62 88 105.62H74.86"
                   stroke="#63666A"
-                  stroke-width="1"
+                  strokeWidth="1"
                   strokeMiterlimit="10"
-                  stroke-linecap="round"
+                  strokeLinecap="round"
                 ></path>
                 <path
                   d="M88.97 52.42H77.33V40.77L88.97 52.42Z"
                   fill="#D7D7D7"
                   stroke="#63666A"
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
                 <path
                   d="M27.32 65.49V70.6"
                   stroke="#D7D7D7"
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
                 <path
                   d="M29.88 68.04H24.76"
                   stroke="#D7D7D7"
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
                 <path
                   d="M110.49 32.5601V39.9901"
                   stroke="#D7D7D7"
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
                 <path
                   d="M114.2 36.27H106.77"
                   stroke="#D7D7D7"
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
                 <path
                   d="M34.07 14.58V25.59"
                   stroke="#D7D7D7"
-                  stroke-width="1"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
                 <path
                   d="M39.57 20.08H28.57"
                   stroke="#D7D7D7"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeWidth="1.5"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                 ></path>
                 <path
                   d="M67 115.86V67.12"
                   stroke="#63666A"
-                  stroke-width="1"
+                  strokeWidth="1"
                   strokeMiterlimit="10"
-                  stroke-linecap="round"
+                  strokeLinecap="round"
                 ></path>
                 <path d="M55.5 78.61L67 67.12L78.5 78.61" fill="white"></path>
                 <path
                   d="M55.5 78.61L67 67.12L78.5 78.61"
                   stroke="#63666A"
-                  stroke-width="1"
+                  strokeWidth="1"
                   strokeMiterlimit="10"
                 ></path>
               </svg>
@@ -474,12 +548,24 @@ function UserPost(props) {
           </span>
         </div>
         <div className={cx("input-user-info")}>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)}/>
-          <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} />
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="text"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
         </div>
         <div className={cx("type-house-title")}>Email</div>
         <div className={cx("input-user-email")}>
-          <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <hr />
         <div className={cx("button-success")}>
