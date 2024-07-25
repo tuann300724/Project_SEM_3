9ec8134af7a3b58f22ba83037ea89ac14fbe996d
tuann300724 <tuann300724@gmail.com>
2024-07-25 21:35:01 +0700

    update

--- src/Admin/Layout/AdminLayout/SidebarAdmin/index.js
+++ src/Admin/Layout/AdminLayout/SidebarAdmin/index.js
@@ -12,83 +12,78 @@ function SidebarAdmin({ isActive }) {
     return (
         <div className={`sidebar ${isActive ? 'active' : ''}`}>
             <div className="logo-details">
-                <i className='bx bxl-c-plus-plus'></i>
+                <i className='bx bxl-c-plus-plus bx-flashing'></i>
                 <span className="logo_name">BatDongSan</span>
             </div>
             <div className="nav-link">
                 <li>
                     <Link className="active" to="/admin">
-                        <i className='bx bx-grid-alt'></i>
+                        <i className='bx bx-grid-alt bx-tada-hover'></i>
                         <span className="links_name">Dashboard</span>
                     </Link>
                 </li>
                 <li>
-                    <Link to="/admin/CDPost">
+                    <Link to="/admin/ListPost">
                         <div className={`nav-item ${isSubMenuOpen ? 'open' : ''}`} onClick={toggleSubMenu}>
-                            <i className='bx bx-box'></i>
+                           <i className='bx bx-box bx-tada-hover'></i> 
                             <span className="links_name">Manager Post</span>
-                            <i className={`bx ${isSubMenuOpen ? 'bx-chevron-down' : 'bx-chevron-right'}`}></i>
+                            <i className={`bx ${isSubMenuOpen ? 'bx-chevron-down bx-fade-down' : 'bx-chevron-right bx-fade-right'}`}></i>
                         </div>
                     </Link>
                     
                 </li>
                 {isSubMenuOpen && (
                         <ul className="sub-menu">
-                            <li><Link to="/admin/CDPost">CDPost</Link></li>
-                            <li><Link to="/admin/AnotherSubLink">Another Sub Link</Link></li>
+                            <li><Link to="/admin/CDPost"><i className="bx bx-loader bx-spin"></i>Processing Post</Link></li>
+                            <li><Link to="/admin/ListPost"><i className='bx bx-list-ul bx-tada-hover'></i>Post</Link></li>
                         </ul>
                     )}
-                <li>
-                    <Link to="/admin/ListPost">
-                        <i className='bx bx-list-ul'></i>
-                        <span className="links_name">Post</span>
-                    </Link>
-                </li>
+              
                 <li>
                     <Link to="/admin/AccountUser">
-                        <i className='bx bx-user'></i>
+                        <i className='bx bx-user  bx-tada-hover'></i>
                         <span className="links_name">User Account</span>
                     </Link>
                 </li>
                 <li>
                     <Link to="/admin/Banner">
-                        <i className='bx bx-pen'></i>
+                        <i className='bx bx-pen  bx-tada-hover'></i>
                         <span className="links_name">Banner design</span>
                     </Link>
                 </li>
                 <li>
                     <Link to="/admin/PackageList">
-                        <i className='bx bx-package'></i>
+                        <i className='bx bx-package  bx-tada-hover'></i>
                         <span className="links_name">Package</span>
                     </Link>
                 </li>
                 <li>
                     <Link to="/admin/Country">
-                        <i className='bi bi-geo-alt'></i>
+                        <i className='bi bi-geo-alt  bx-tada-hover'></i>
                         <span className="links_name">Country</span>
                     </Link>
                 </li>
                 <li>
                     <Link to="/admin/Feedback">
-                        <i className='bx bx-message'></i>
+                        <i className='bx bx-message  bx-tada-hover'></i>
                         <span className="links_name">Messages</span>
                     </Link>
                 </li>
                 <li>
                     <Link to="/admin/NewAdmin">
-                        <i className='bx bx-heart'></i>
+                        <i className='bx bx-heart  bx-tada-hover'></i>
                         <span className="links_name">New</span>
                     </Link>
                 </li>
                 <li>
                     <a href="#">
-                        <i className='bx bx-cog'></i>
-                        <span className="links_name">Setting</span>
+                        <i className='bx bx-cog  bx-spin-hover'></i>
+                        <span className="links_name ">Setting</span>
                     </a>
                 </li>
                 <li >
                     <a href="#">
-                        <i className='bx bx-log-out'></i>
+                        <i className='bx bx-log-out  bx-tada-hover'></i>
                         <span className="links_name">Log out</span>
                     </a>
                 </li>
