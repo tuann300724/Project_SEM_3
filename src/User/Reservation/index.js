4ef9837329a5aaf7d53acf12987dd74306df76c5
IYC054 <trieunhananh@gmail.com>
2024-08-04 16:23:31 +0700

    iyc add thu vien react-hook-form

--- src/User/Reservation/index.js
+++ src/User/Reservation/index.js
@@ -1,4 +1,9 @@
 import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { v4 as uuid } from "uuid";
+import * as yup from "yup";
 
 function Reservation(props) {
     return (
